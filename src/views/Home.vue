a4755bd44efbb49efb27f28b72661f2f34797d10
mzrar-myrtle <mzrar.asus@gmail.com>
2024-07-11 15:28:41 +0300

    Feat(./): Configuring the statemangement. pinia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from "../store/counter";
+
+const counterStore = useCounterStore();
+</script>
+
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="tw-grid tw-justify-center">
+    <h1 class="tw-text-xl">Home {{ counterStore.count }}</h1>
+    <button @click="counterStore.increment">Increment</button>
   </div>
 </template>
